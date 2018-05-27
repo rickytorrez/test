203537d5827b093c55a3d6d07654e693f702941f
rickytorrez <torrez.ricky@ymail.com>
2018-05-26 22:22:31 -0400

    first commit

---
+++ dcHome/src/main/java/com/ericardo/dcHome/DcHomeApplication.java
@@ -0,0 +1,12 @@
+package com.ericardo.dcHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcHomeApplication.class, args);
+	}
+}
