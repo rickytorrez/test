7a4e138c0d128f9d8bc9a3db753624d1b16dd007
rickytorrez <torrez.ricky@ymail.com>
2018-05-31 10:59:11 -0400

    Listing Posting to DB

--- dcHome/src/main/java/com/ericardo/dcHome/controllers/ListingController.java
+++ dcHome/src/main/java/com/ericardo/dcHome/controllers/ListingController.java
@@ -1,11 +1,16 @@
 package com.ericardo.dcHome.controllers;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ericardo.dcHome.models.Listing;
 import com.ericardo.dcHome.models.User;
 import com.ericardo.dcHome.services.ListingService;
 import com.ericardo.dcHome.services.UserService;
@@ -32,5 +37,44 @@ public class ListingController {
 		}
 		_model.addAttribute("listings", _lS.all());
 		return "listing";
+	}
+	
+	@RequestMapping("/realtor")
+	public String realtor(HttpSession _session, Model _model, @ModelAttribute("listing") Listing listing) {
+		if(_session.getAttribute("id") == null) {
+			return "redirec:/users/new";
+		}
+		
+		User user = _uS.find( (Long) _session.getAttribute("id"));
+		
+		if(!user.isRealtor() && !user.isAdmin()) {
+			return "redirect:/listings";
+		}
+		
+		_model.addAttribute("user", user);
+		_model.addAttribute("listings", new Listing());
+		
+		return "realtor";		
+	}
+	
+	@PostMapping("/createListing")
+	public String create(@Valid @ModelAttribute("listing") Listing listing, BindingResult _result, HttpSession _session) {
+		if(_session.getAttribute("id") == null ) {
+			return "redirect:/users/new";
+		}
+		
+		User user = _uS.find( (Long) _session.getAttribute("id") );
+		
+		if(!user.isRealtor() && !user.isAdmin()) {
+			return "redirect:/listings";
+		} else {
+			if(_result.hasErrors()) {
+				return "/listings/realtor";
+			} else {
+				listing.setUser(user);
+				_lS.create(listing);
+				return "redirect:/listings/realtor";
+			}
+		}
 	}
 }
